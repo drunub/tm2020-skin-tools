ec2e9fec5b45e3304e61e5ba7a6ebd6489037e82
drunub <161570412+drunub@users.noreply.github.com>
2026-07-05 03:27:13 +0100

    light stuff

--- README.md
+++ README.md
@@ -1,23 +1,87 @@
 # tm2020-skin-tools
+
 documentation of findings and collection of tools/scripts i created that allow and ease the creation of 3d trackmania 2020 skins
+
+**most of this isn't official or standard (especially what skinfix.py does), it was just a hacky way to get 3d skins working. it might not work forever, it might crash your game etc. please don't abuse it****
+
+it's possible the use or behaviour of these will change in the future, but i will try to keep the readme up to date
 
 i will release a tutorial eventually in document and video form
 
-you should also check out [bmx22c's tutorial](https://www.youtube.com/watch?v=yXS1dwcq1Cs), you can incorporate the tools here with it
+you should also check out [bmx22c's tutorial](https://www.youtube.com/watch?v=yXS1dwcq1Cs) and [bmx's skinmaker](https://github.com/bmx22c/SkinMaker), you can incorporate the tools here with it. skinmaker comes with skinfix and automates tedious stuff with nadeoimporter and xml files.
 
 thanks to bmx22c for the original export technique and helping with stuff and thx to the schadocalex for gbx-py
 
 all these tools are intended to be used with each other. boner.py and fakeshad.py are optional, but i seriously recommend boner.py, it will make ur life a lot easier
 
-it's possible the specific use or behaviour of these will change in the future, but i will keep the readme up to date
-
 the blender scripts were created with blender 4.3 nightly, but the 4.x versions before will work, and probably some of the later 3.x versions
-
-damage models should be possible, but not sure exactly how atm (i think it's the secondary positions "Position1" in the vertex streams)
 
 if you have any questions pls feel free to message me on telegram, twitter, or discord by the same name
 
+## skinfix.py
+
+until nadeo release tools or documentation for 3d skin creation, this is the only way we know how to create textured 3d car skins. nadeoimporter might have the functionality to do it completely but it is undocumented or inaccessible.
+
+right now,  you can create a 3d skin with textures, pilot/driver, fakeshad (baked shadow), standard tmf/tm2 skeleton (and multiple exhaust flames), and customisable lights (colour, point/spot, angles, etc)
+
+in the future, it should be possible to make a damage model, i also see fields for UV animations, and other weird stuff. it's possible the use the extra tm2020-specific bones but it's a massive headache rn, it requires some extra steps and it's janky.
+
+
+
+there is a standalone skinfix executable in the releases. if you don't want to use that, you need:
+
+- python 3.11
+- [gbx-py (my fork)](https://github.com/drunub/gbx-py)
+- construct
+
+
+
+once you've exported your mesh with nadeoimporter, you can use this tool on it with the command (or simply drag the gbx onto the script):
+
+`skinfix.py mainbody.mesh.gbx` (alternatively .exe if you have the packaged version)
+
+the input file will be overwritten unless specified with the `-o FILE` option, e.g.
+
+`skinfix.py mainbody.mesh.gbx -o out.mesh.gbx`
+
+custom lod distances can be specified with `-l LOD1 LOD2 LOD3` e.g:
+
+`skinfix.py mainbody.mesh.gbx -l 10 20 40 80`
+
+i don't recommend changing lod dists, and you shouldn't use more than 5 LODs
+
+honestly just use 3 LODs, it looks fine, and you don't have to worry about armature LODs (certain bones are unused at certain LODs)
+
+#### new: lights
+
+lights can now be added using sockets named in the format:
+
+`Light1_R1.0_B0_G0_I10_D10_A45_AI30_N` (sockets are usually created by prefixing bone names with an underscore or hash, e.g. _Light1..., check your MeshParams xml).
+
+at the very minimum, the socket needs to be named `Light1`, `Light2`, etc. the number is just to prevent duplicate names in blender, but it must be there.
+
+all the fields are optional with sensible defaults:
+
+`R`,`G`,`B` - colour, float 0.0 to 1.0
+
+`I` - intensity
+
+`D` - distance
+
+`A` - outer angle, degrees - use to make it a spot light, omit to keep it as a point light
+
+`AI` - inner angle - for spot light, omit for an automatic value
+
+`N` - night-only, will turn off in day mood
+
+#### fakeshad
+
+to create a fakeshad, create a thin cuboid that intersects the Z plane and leave a little allowance for the shadow. this will be the fakeshad bounds. assign the texture `SkinDmg_fakeshad` so skinfix.py can tell what it is. you can then add fakeshad.dds texture to your skin zip. 
+
+i made a blender script to automate the fakeshad bake and cuboid creation if you need :0)
+
 ## blender/boner.py
+
 completely automates the armature creation for a 3d car skin
 
 **tldr: make a 3d skin the tm2 way, select all parts, run script, and then you should be good for next step**
@@ -49,6 +113,7 @@ sockets use underscore prefix, and will create the same name for the bone, so en
 in tm2020, the only sockets i'm aware of that work are LightFProj and Exhaust1/Exhaust2, the rest are just listed to ensure the meshes don't sneak through. sockets are basically the equivalent to empties in tmf/tm2
 
 ## blender/fakeshad.py
+
 automatically create fakeshad mesh and texture
 
 the fakeshad in 2020 is defined by a bounding box, which can be converted from mesh to bounding box by skinfix.py. fakeshad is optional but baked shadows look cool :0)
@@ -61,30 +126,3 @@ the width and length of the fakeshad bounds can be controlled by ``FS_W`` and ``
 you should set ``BAKE_TYPE`` to ``"AO"`` or ``"SHADOW"``. ambient occlusion bake is noisy but much faster.
 
 blender will freeze during the bake. you should configure the cycles samples and device in the render properties, use GPU if available
-
-
-## skinfix.py
-until nadeo release tools or documentation for skin creation, this is needed for textures and fakeshad to work
-
-requirements:
-- python 3.11
-- gbx-py (dev branch)
-- construct
-
-i will release an executable packaged with python runtime, etc so you dont have to install dependencies
-
-once you've exported your mesh with nadeoimporter, you can use this tool on it with the command (or simply drag the gbx onto the script):
-
-``skinfix.py mainbody.mesh.gbx`` (alternatively .exe if you have the packaged version)
-
-the input file will be overwritten unless specified with the ``-o FILE`` option, e.g. 
-
-``skinfix.py mainbody.mesh.gbx -o out.mesh.gbx``
-
-custom lod distances can be specified with ``-l LOD1 LOD2 LOD3`` e.g:
-
-``skinfix.py mainbody.mesh.gbx -l 10 20 40 80``
-
-i don't recommend changing lod dists, and you shouldn't use more than 5 LODs
-
-honestly just use 3 LODs, it looks fine, and you don't have to worry about armature LODs (certain bones are unused at certain LODs)
--- skinfix.py
+++ skinfix.py
@@ -1,12 +1,71 @@
-from gbx_py.gbx_structs import GbxStruct, GbxStructWithoutBodyParsed
-from gbx_py.parser import parse_file, generate_file
+from gbx_py_dev.src.gbx_structs import GbxStruct, GbxStructWithoutBodyParsed, NodeRef
+from gbx_py_dev.src.parser import parse_file, generate_file
 from construct import ListContainer, Container, Array
+from collections import OrderedDict
+import sys
 import re
 import argparse
-from math import log2
-import sys
 
-mat_pattern = re.compile(r"(?:_lod(\d+))?(_?)$")
+pattern_mat = re.compile(r'^(?P<mat>.+?)(?:_lod(?P<lod>\d+))?(?P<damage>_)?$', re.IGNORECASE)
+pattern_light = re.compile(r'^light\d+', re.IGNORECASE)
+
+
+def light_user_model(name):
+    light = Container(
+            version=1,
+            Spot=False,
+            Color=Container(x=1.0, y=1.0, z=1.0),
+            Intensity=10.0,
+            Distance=10.0,
+            PointEmissionRadius=0.0, #?
+            PointEmissionLength=0.0, #
+            SpotInnerAngle=0,
+            SpotOuterAngle=45.0,
+            SpotEmissionSizeX=0.0, #
+            SpotEmissionSizeY=0.0, #
+            NightOnly=False,
+    )
+    
+    parts = re.findall(r"_([a-z]+)(\d+\.?\d*)?", name.lower())
+    
+    for k, v in parts:
+        if k == "r":
+            light.Color.x = float(v)
+        elif k == "g":
+            light.Color.y = float(v)
+        elif k == "b":
+            light.Color.z = float(v)
+        elif k == "i":
+            light.Intensity = float(v)
+        elif k == "d":
+            light.Distance = float(v)
+        elif k == "ao" or k == "a":
+            light.Spot = True
+            light.SpotOuterAngle = float(v)
+        elif k == "ai":
+            light.SpotInnerAngle = float(v)
+        elif k == "n":
+            light.NightOnly = True
+    
+    #print(light)
+    #input()
+    
+    if not light.SpotInnerAngle:
+        light.SpotInnerAngle = light.SpotOuterAngle * 0.7
+    
+    body = OrderedDict(
+        [
+            (
+                0x090F9000,
+                light,
+            ),
+            (0xFACADE01, None),  # end-of-node marker
+        ]
+    )
+    
+    print("Light -", name)
+    return NodeRef(0x090F9000, body)
+
 
 
 def fakeshad_bounds(**kwargs):
@@ -46,115 +105,124 @@ def get_bbox(verts):
     )
 
 
-
 # penis
-LOD_DISTS = [[], # used for 1 lod
-             [80],
-             [40, 80], # lagoon/tm2 lod dists
-             [10, 40, 80],
-             [4, 10, 40, 80]] # 2020 lod dists - used for 5 lods
+LOD_DISTS = [4, 10, 40, 80]
              
 
-arg_parser = argparse.ArgumentParser()
+def fix_skin(file_in=None, file_out=None, lod_dists=None):
+    file_out = file_out or file_in
 
-arg_parser.add_argument('file',
-                    metavar="IN_FILE",
-                    type=str,
-                    help="Input file")
+    data = parse_file(file_in)
+    
+    solid2 = data.body[0x90bb000]
+    fakeshad = data.body[0x90bb002]
+    skel = solid2.skel.body[0x90ba000]
 
-arg_parser.add_argument("-l", "--lod-dists", 
-                        dest="lod_dists",
-                        metavar="LOD_DIST",
-                        nargs="+",
-                        type=float,
-                        help='List of lod distances')
+    solid2.materialsNames = []
 
-arg_parser.add_argument("-o", "--out", 
-                        dest="out_path",
-                        metavar='OUT_FILE',
+    custom_mats = [m.materialUserInst.body[0x90fd000].materialName for m in solid2.customMaterials]
+    
+    lods = set()
+
+    if not solid2.customMaterials:
+        print("Nothing to do - no custom materials")
+        return
+
+
+    for g in solid2.shadedGeoms:
+        mat_idx = g.materialIndex
+        
+        mat = solid2.customMaterials[mat_idx].materialUserInst.body[0x90fd000].materialName
+        vis_idx = g.visualIndex
+        visual = solid2.visuals[vis_idx]
+        vert_count = visual.body[0x0900600F].VertexCount
+        
+        m = pattern_mat.match(mat)
+        mat, lod, is_damage = m.group('mat'), m.group('lod'), m.group('damage')
+        
+        if is_damage: # unimplemented
+            print(f"Visual {vis_idx:3} - Removed damage mesh")
+            solid2.shadedGeoms.remove(g)
+            continue
+        
+        if mat.lower().startswith("fakeshad"):
+            print(f"Visual {vis_idx:3} - Fakeshad")
+            solid2.shadedGeoms.remove(g)
+            bbox = get_bbox(visual.body[0x0900600f].vertexStreams[0].body[0x9056000].Data[0])
+            fakeshad.u01 = fakeshad_bounds(**bbox)
+            continue
+        
+        if lod:
+            lod = int(lod)
+            lod = 2 ** (lod - 1)
+            g.lod = lod
+        
+        
+        print(f"Visual {vis_idx:3} - lod {g.lod:3} - {vert_count:6} verts - {mat}")
+        
+        lods.add(g.lod)
+        
+        mat= "_" + mat
+        
+        if mat in solid2.materialsNames:
+            g.materialIndex = solid2.materialsNames.index(mat)
+        else:
+            g.materialIndex = len(solid2.materialsNames)
+            solid2.materialsNames.append(mat)
+    
+    
+    for sock_idx, sock in enumerate(skel.sockets):
+        if pattern_light.match(sock.name):
+            light_model = light_user_model(sock.name)
+            if light_model in solid2.lightUserModels:
+                model_index = solid2.lightUserModels.index(light_model)
+            else:
+                solid2.lightUserModels.append(light_model)
+                model_index = len(solid2.lightUserModels) - 1
+            solid2.lightInsts.append(
+                Container(modelIndex=model_index, socketIndex=sock_idx)
+            )
+    
+    solid2.lodDistances = lod_dists[4-min(4, len(lods)):]
+
+    solid2.listVersion02 = 10
+    solid2.materials = []
+    solid2.materialCount = 0
+    solid2.customMaterials = []
+    solid2.u14 = -1
+    
+    new_bytes = generate_file(data, reindex_nodes=True)
+    
+    open(file_out, "wb").write(new_bytes)
+
+
+def parse_args():
+    arg_parser = argparse.ArgumentParser()
+    
+    arg_parser.add_argument('file_in',
+                        metavar="IN_FILE",
                         type=str,
-                        help='Output file path, write to input file if empty')
-
-args = arg_parser.parse_args()
-
-
-lod_dists = args.lod_dists or LOD_DISTS
-
-file_path = args.file
-file_path_out = args.out_path or file_path
-
-data = parse_file(file_path)
-
-
-solid2 = data.body[0x90bb000]
-fakeshad = data.body[0x90bb002]
-skel = solid2.skel.body[0x90ba000]
-
-solid2.materialsNames = []
-
-custom_mats = [m.materialUserInst.body[0x90fd000].materialName for m in solid2.customMaterials]
-
-
-lods = set()
-
-if not solid2.customMaterials:
-    print("Nothing to do - no custom materials")
-    sys.exit()
-
-
-for g in solid2.shadedGeoms[:]:
-    mat_idx = g.materialIndex
+                        help="Input file")
     
-    mat = solid2.customMaterials[mat_idx].materialUserInst.body[0x90fd000].materialName
-    vis_idx = g.visualIndex
-    visual = solid2.visuals[vis_idx]
-    vert_count = visual.body[0x0900600F].VertexCount
+    arg_parser.add_argument("-l", "--lod-dists", 
+                            dest="lod_dists",
+                            metavar="LOD_DIST",
+                            nargs="+",
+                            default=LOD_DISTS,
+                            type=float,
+                            help='List of lod distances')
     
+    arg_parser.add_argument("-o", "--out", 
+                            dest="file_out",
+                            metavar='OUT_FILE',
+                            type=str,
+                            help='Output file path, write to input file if empty')
     
-    mat_data = mat_pattern.search(mat)
-    lod, is_damage = mat_data.group(1), bool(mat_data.group(2))
-    mat = mat_pattern.sub("", mat)
-    
-    if is_damage: # unimplemented
-        print(f"Visual {vis_idx:3} - Removed damage mesh")
-        solid2.shadedGeoms.remove(g)
-        continue
-    
-    if mat.lower().startswith("fakeshad"):
-        print(f"Visual {vis_idx:3} - Fakeshad")
-        solid2.shadedGeoms.remove(g)
-        bbox = get_bbox(visual.body[0x0900600f].vertexStreams[0].body[0x9056000].Data[0])
-        fakeshad.u01 = fakeshad_bounds(**bbox)
-        continue
-    
-    if lod:
-        lod = int(lod)
-        lod = 2 ** (lod - 1)
-        g.lod = lod
-    
-    
-    print(f"Visual {vis_idx:3} - lod {g.lod:3} - {vert_count:6} verts - {mat}")
-    
-    lods.add(g.lod)
-    
-    mat= "_" + mat
-    
-    if mat in solid2.materialsNames:
-        g.materialIndex = solid2.materialsNames.index(mat)
-    else:
-        g.materialIndex = len(solid2.materialsNames)
-        solid2.materialsNames.append(mat)
+    return arg_parser.parse_args()
 
 
-solid2.lodDistances = LOD_DISTS[len(lods)-1] # car will go disappear if mismatched
 
-solid2.listVersion02 = 10
-solid2.materials = []
-solid2.materialCount = 0
-solid2.customMaterials = []
-solid2.u14 = -1
-
-
-new_bytes = generate_file(data)
-
-open(file_path_out, "wb").write(new_bytes)
+if __name__ == "__main__":
+    args = vars(parse_args())
+    
+    sys.exit(fix_skin(**args))
